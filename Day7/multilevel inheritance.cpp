5ce62c6904341ec2a86726a0208078bbea485d94
solankiyashvi1807 <solankiyashvi1807@gmail.com>
2026-05-07 15:07:14 +0530

    C++ codes

---
+++ Day7/multilevel inheritance.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Player
+{
+public:
+    string name;
+    int runs;
+
+    void setName(string n)
+    {
+        name = n;
+    }
+    void setRuns(int r)
+    {
+        runs = r;
+    }
+};
+
+class Cricketer : public Player
+{
+public:
+    string country;
+    void setCountry(string s)
+    {
+        country = s;
+    }
+};
+
+
+class Batter : public Cricketer
+{
+public:
+    int centuries;
+    void setCenturies(int c)
+    {
+        centuries = c;
+    }
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Runs: " << runs << endl;
+        cout << "Country: " << country << endl;
+        cout << "Centuries: " << centuries << endl;
+    }
+};
+int main()
+{
+    int runs;
+    cin >> runs;
+    Batter c1;
+    c1.setCountry("India");
+    c1.setName("Virat kohli");
+    c1.setRuns(runs);
+    c1.setCenturies(43);
+    c1.show();
+    return 0;
+}
