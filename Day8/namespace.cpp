af0264919b0db725080c4d71168e81532440a6ea
solankiyashvi1807 <solankiyashvi1807@gmail.com>
2026-05-10 13:04:04 +0530

    c++ codes

---
+++ Day8/namespace.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+namespace Virat
+{
+
+    void show()
+    {
+        cout << "Virat is the part of rcb" << endl;
+
+    }
+}
+namespace Rohit
+{
+    void show()
+    {
+        cout << "Rohit is the part of mi" << endl;
+
+    } ppppppppppp'
+
+    p
+}
---
+++ Day8/static member function.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+public:
+    static int 
+
+
+
+
+
+
+    int main()
+    {
+        student s1;
+        student s2;
+        student s3;
+        student 
+
+    }
+}
